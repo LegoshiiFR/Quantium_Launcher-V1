b0df856a163e7323da6be572bc0a395e076faca0
LegoshiiFR <youtune.contact.rageman@gmail.com>
2024-05-15 15:31:52 +0200

    Publish V1.0.2

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,11 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je suis une énergie.", "author": "Quantium" },
+            { "message": "J'aime la programmation.", "author": "Quantium" },
+            { "message": "Suis-je un humain ?", "author": "Quantium" },
+            { "message": "Cube", "author": "Quantium" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Quantium" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
